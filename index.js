7c79f8e7af5a06df02f3d36876d15bc6cef5bd88
Jerome Terry <jerome.l.terry@gmail.com>
2015-12-15 22:00:03 -0330

    Adding Express JS

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-//Note: This file is provided as an aid to help you get up and running with
-//Electron for desktop apps. See the readme file for more information.
-/* eslint-disable strict, no-var, no-console */
+var express = require('express');
+var path = path = require('path');
+var ejs = require('ejs');
 
-'use strict';
+var app = express();
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
-var mainWindow = null;
+app.use(express.static(path.join(__dirname, 'public/ember-app')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-require('crash-reporter').start();
-
-app.on('window-all-closed', function() {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.get("/", function(req, res) {
+	res.sendFile("index.html", { root: "./public/ember-app/" });
 });
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
-  });
-
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  mainWindow.webContents.on('did-finish-load', function() {
-    mainWindow.setTitle(app.getName());
-  });
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
+
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+module.exports = app;
